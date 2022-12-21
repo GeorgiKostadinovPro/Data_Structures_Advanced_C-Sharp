7627f18f3d54f230ff6f9316a4b6c9d03f916ddb
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-12-21 21:17:50 +0200

    2-3 Trees implementation using generic class

---
+++ 01_B-Trees_2-3_Trees_And_Red-Black/Lab/Demo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using _01.Two_Three;
+
+namespace Demo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var tree = new TwoThreeTree<string>();
+
+            tree.Insert("A");
+            tree.Insert("E");
+            tree.Insert("D");
+            tree.Insert("B");
+            tree.Insert("F");
+            tree.Insert("G");
+            tree.Insert("C");
+
+            Console.WriteLine(tree);
+        }
+
+        private class IntDemo : IComparable<IntDemo>
+        {
+            public IntDemo(int value)
+            {
+                this.Value = value;
+            }
+
+            public int Value { get; set; }
+
+            public int CompareTo([AllowNull] IntDemo other)
+            {
+                return this.Value.CompareTo(other.Value);
+            }
+
+            public override string ToString()
+            {
+                return this.Value.ToString();
+            }
+        }
+    }
+}
