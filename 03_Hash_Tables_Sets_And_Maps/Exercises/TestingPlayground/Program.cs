add2d1a0a76fc1bec9d13848d5b92ae0e70da405
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2023-01-31 20:29:22 +0200

    unit tests are passing 100 / 100

---
+++ 03_Hash_Tables_Sets_And_Maps/Exercises/TestingPlayground/Program.cs
@@ -0,0 +1,25 @@
+﻿using RoyaleArena;
+using System;
+
+namespace TestingPlayground
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IArena RA = new Arena();
+            BattleCard cd1 = new BattleCard(5, CardType.SPELL, "joro", 10, 5);
+            BattleCard cd2 = new BattleCard(6, CardType.SPELL, "joro", 10, 5);
+            BattleCard cd3 = new BattleCard(7, CardType.SPELL, "joro", 10, 5);
+
+            RA.Add(cd1);
+            RA.Add(cd2);
+            RA.Add(cd3);
+            RA.RemoveById(5);
+
+            var card = RA.GetById(5);
+
+            Console.WriteLine(card.Id);
+        }
+    }
+}
---
+++ 03_Hash_Tables_Sets_And_Maps/Exercises/WordCruncher/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordCruncher
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] words = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string targetString = Console.ReadLine();
+
+            WordCruncher wordCruncher = new WordCruncher(words, targetString);
+
+            foreach (var path in wordCruncher.GetSyllablePaths())
+            {
+                Console.WriteLine(string.Join(" ", path));
+            }
+        }
+    }
+
+    public class WordCruncher
+    {
+        private class Node
+        {
+            public Node(string syllable, IList<Node> nextSyllables)
+            {
+                this.Syllable = syllable;
+                this.NextSyllables = nextSyllables;
+            }
+
+            public string Syllable { get; }
+
+            public IList<Node> NextSyllables { get; }
+        }
+
+        private readonly IList<Node> syllableGroups;
+
+        public WordCruncher(string[] words, string targetString)
+        {
+            this.syllableGroups = this.GenerateSyllableGroups(words, targetString);
+        }
+
+        private IList<Node> GenerateSyllableGroups(string[] words, string targetString)
+        {
+            if (string.IsNullOrEmpty(targetString)
+                || words.Length == 0)
+            {
+                return null;
+            }
+
+            var resultValues = new List<Node>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                var syllable = words[i];
+
+                if (targetString.StartsWith(syllable))
+                {
+                    var nextSyllables = this.GenerateSyllableGroups(
+                        words.Where((_, index) => index != i).ToArray(),
+                        targetString.Substring(syllable.Length));
+                        
+                    Node node = new Node(syllable, nextSyllables);
+                    resultValues.Add(node);
+                }
+            }
+
+            return resultValues;
+        }
+
+        public IEnumerable<string> GetSyllablePaths()
+        {
+            var allPaths = new List<List<string>>();
+
+            this.GeneratePaths(this.syllableGroups, new List<string>(), allPaths);
+
+            return new HashSet<string>(allPaths.Select(path => string.Join(" ", path)));
+        }
+
+        private void GeneratePaths(IList<Node> syllableGroups, List<string> currentPath, List<List<string>> allPaths)
+        {
+            if (syllableGroups == null)
+            {
+                allPaths.Add(new List<string>(currentPath));
+                return;
+            }
+
+            foreach (var node in syllableGroups)
+            {
+                currentPath.Add(node.Syllable);
+
+                this.GeneratePaths(node.NextSyllables, currentPath, allPaths);
+
+                currentPath.RemoveAt(currentPath.Count - 1);
+            }
+        }
+    } 
+}
